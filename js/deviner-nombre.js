c8deea899181b28fc8657c832df6d0ec53144d51
Rondez Rayan <rayan.rondez@divtec.ch>
2022-08-30 11:06:00 +0200

    Bientôt terminé

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,14 +1,14 @@
 /**
 * Jeu du nombre mystère
-* @author  Steve Fallet
+* @author  Rayan Rondez
 * @version 0.1
-* @since   2018-09-09 (date de création)
+* @since   2022-08-30
 */
-
+const MAX_VAL = 100;
+const MIN_VAL = 1;
 // Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
-    "use strict";
-
+    'use strict';
     /**
      * Retourne un nombre entier aléatoire compris entre min et max
      * @param min
@@ -18,5 +18,21 @@
     function tireNombre(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
-
+    let nbMystere = tireNombre(MIN_VAL, MAX_VAL);
+    let mystereTrouver = false;
+    do{
+    let reponse = prompt('Le nombre à deviner est de 1 à 100');
+    let nbEssais;
+    nbEssais += 1;
+    if (reponse === nbMystere){
+    alert('Vous avez trouvé après ${nbEssais} d´essais');
+    mystereTrouver = true;
+    } else{
+        if (reponse>nbMystere){
+            alert('C´est moins !');
+        }else{
+            alert('C´est plus !');
+        }
+    }
+    }while(mystereTrouver === false);
 }()); // main IIFE
